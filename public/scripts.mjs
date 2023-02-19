09ad5efadfaaa1ccc9f31a7277df157676ca7807
Todd Alloway <kinome79@yahoo.com>
2023-02-18 20:40:54 -0500

    changes to correct font issue

--- public/scripts.mjs
+++ public/scripts.mjs
@@ -9,14 +9,24 @@ const context = canvas.getContext('2d');
 //const socket = io();
 
 // Attempt to preload font - still not working 
-const myfont = new FontFace('Press Start', 'url(https://fonts.gstatic.com/s/pressstart2p/v14/e3t4euO8T-267oIAQAu6jDQyK3nRivN04w.woff2)');
-document.fonts.add(myfont);
-myfont.load().then( () => {
-    context.font = "24px 'Press Start 2P'";
-    context.fillStyle = 'black';
-    context.fillText ("This is just a test", 100, 100);
-    context.font = "16px 'Press Start 2P'";
-    context.fillText ("I would be interested to know", 100, 170);
-    context.fillText ("if this actually works.", 100, 230);
-});
+//const myfont = new FontFace('Press Start 2P', 'url(https://fonts.gstatic.com/s/pressstart2p/v14/e3t4euO8T-267oIAQAu6jDQyK3nRivN04w.woff2)');
+//document.fonts.add(myfont);
+//myfont.load().then( () => {});
+//document.fonts.ready.then (() => {
+    drawTitle();
+//});
 
+
+
+
+
+function drawTitle() {
+context.font = "16px 'Press Start 2P'"; context.fillText("hello", 50,50);
+context.font = "24px 'Press Start 2P'";
+context.fillStyle = 'black';
+context.fillText ("This is just a test", 100, 100);
+context.font = "16px 'Press Start 2P'";
+context.fillText ("I would be interested to know", 100, 170);
+context.fillText ("if this actually works.", 100, 230);
+
+}
